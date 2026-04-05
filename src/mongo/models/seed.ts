d2d81c891771608e12d39839d40cce4dd60accae
Hayden <hayden.clark@mymail.champlain.edu>
2026-04-05 19:57:11 -0400

    Add mongo seed data

---
+++ src/mongo/models/seed.ts
@@ -0,0 +1,21 @@
+import {connectToMongo, disconnectMongo} from "../db.ts";
+import {Transaction} from "./transaction";
+
+await connectToMongo();
+await Transaction.insertOne({
+    recipients: [2],
+    content: "This is an example transaction.",
+    sender: 1,
+    amount: 150.00,
+    visibility: "public"
+})
+await Transaction.insertOne({
+    recipients: [1,3],
+    content: "This is a second example transaction.",
+    sender: 2,
+    amount: 50.00,
+    visibility: "public"
+})
+
+
+await disconnectMongo();
