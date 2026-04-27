abf0b03ab121728ecdb561c47527855b69e238d5
Hayden <hayden.clark@mymail.champlain.edu>
2026-04-26 23:56:24 -0400

    Update SSN schema for bcrypt

--- src/postgres/schema.ts
+++ src/postgres/schema.ts
@@ -8,7 +8,7 @@ export const usersTable = pgTable("users", {
     last_name: varchar({ length: 255 }).notNull(),
     email: varchar({ length: 255 }).notNull().unique(),
     balance: decimal({ precision: 19, scale: 2 }).default("15.00").notNull(),
-    ssn_hash: char({ length: 64 }).notNull().unique(),
+    ssn_hash: varchar({ length: 255 }).notNull().unique(),
     status: accountStatusEnum().default("active").notNull(),
     password_hash: varchar({ length: 255 }).notNull(),
 });
